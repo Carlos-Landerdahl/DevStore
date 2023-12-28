3a58763543e28d63d26f10e2bf0ce5b5062012a3
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-12-28 10:47:37 -0300

    feat: dynamic metadata

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 
 interface ProductsProps {
@@ -18,6 +19,16 @@ async function getProducts(slug: string): Promise<Product> {
   const produtcts = await response.json()
 
   return produtcts
+}
+
+export async function generateMetadata({
+  params,
+}: ProductsProps): Promise<Metadata> {
+  const product = await getProducts(params.slug)
+
+  return {
+    title: product.title,
+  }
 }
 
 export default async function ProductPage({ params }: ProductsProps) {
